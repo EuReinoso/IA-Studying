9b8551e8f7e737b0e8a71d54d769f92cb5cec584
EuReinoso <lucas.reinoso.br@gmail.com>
2021-03-17 09:23:38 -0300

    generate data

---
+++ AlgorítimoOtimizacao/main.py
@@ -0,0 +1,13 @@
+
+
+
+flights_data = {}
+def gen_data(data,path):
+    for line in open(path):
+        origin,destination,out_time,arrival_time,price = line.split(',')
+        data.setdefault((origin,destination),[])
+        data[(origin,destination)].append((out_time,arrival_time,int(price)))
+    
+gen_data(flights_data,'AlgorítimoOtimizacao/flights.txt')
+
+print(flights_data)
