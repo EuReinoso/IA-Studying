5ced546dcdf6540354fad79d4f48603355d46666
EuReinoso <lucas.reinoso.br@gmail.com>
2021-03-18 09:45:38 -0300

    Print Flights

--- AlgorítimoOtimizacao/main.py
+++ AlgorítimoOtimizacao/main.py
@@ -6,7 +6,7 @@ people = [('Lisboa', 'LIS'),
         ('Bruxelas', 'BRU'),
         ('Londres', 'LHR')]
 
-flights_data = {}
+
 
 def gen_data(path):
     data = {}
@@ -16,8 +16,25 @@ def gen_data(path):
         data[(origin, destination)].append((out_time, arrival_time, int(price)))
     return data
 
+def print_flights(schedule,data):
+    index = -1
+    total_price = 0
+    for i in range(len(schedule)//2):
+        name = people[i][0]
+        origin = people[i][1]
+        index += 1
+        out = data[(origin,destination)][schedule[index]]
+        total_price += out[2]
+        index += 1
+        arrival = data[(destination,origin)][schedule[index]]
+        total_price += arrival[2]
+        print('%10s%10s %5s-%5s %3s %5s-%5s %3s' %(name,origin,out[0],out[1],out[2],
+                                                    arrival[0],arrival[1],arrival[2]))
+    print("Total Price:", total_price)
 
+flights_data = {}
 flights_data = gen_data('AlgorítimoOtimizacao/flights.txt')
+destination = 'FCO'
+schedule = [1,0, 3,2, 7,1, 6,3, 2,4, 5,3]
 
-
-print(flights_data)
+print_flights(schedule,flights_data)
