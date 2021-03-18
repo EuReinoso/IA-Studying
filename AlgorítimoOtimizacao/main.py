734a9234016caca4cc903b4b9b10b3352d486599
EuReinoso <lucas.reinoso.br@gmail.com>
2021-03-18 09:15:24 -0300

    Modifying gen_data

--- AlgorítimoOtimizacao/main.py
+++ AlgorítimoOtimizacao/main.py
@@ -1,18 +1,23 @@
 
-people = [('Lisboa','LIS'),
-        ('Madrid','MAD'),
-        ('Paris','CDG'),
-        ('Dublin','DUB'),
-        ('Bruxelas','BRU'),
-        ('Londres','LHR')]
+people = [('Lisboa', 'LIS'),
+        ('Madrid', 'MAD'),
+        ('Paris', 'CDG'),
+        ('Dublin', 'DUB'),
+        ('Bruxelas', 'BRU'),
+        ('Londres', 'LHR')]
 
 flights_data = {}
-def gen_data(data,path):
+
+def gen_data(path):
+    data = {}
     for line in open(path):
-        origin,destination,out_time,arrival_time,price = line.split(',')
-        data.setdefault((origin,destination),[])
-        data[(origin,destination)].append((out_time,arrival_time,int(price)))
-    
-gen_data(flights_data,'AlgorítimoOtimizacao/flights.txt')
+        origin, destination, out_time, arrival_time, price = line.split(',')
+        data.setdefault((origin, destination), [])
+        data[(origin, destination)].append((out_time, arrival_time, int(price)))
+    return data
+
+
+flights_data = gen_data('AlgorítimoOtimizacao/flights.txt')
+
 
 print(flights_data)
