6631f8ebb37594ebecc42bda627b230a228bb849
EuReinoso <lucas.reinoso.br@gmail.com>
2021-03-19 12:25:25 -0300

    best individual

--- AlgorítimoOtimizacao/Genético.py
+++ AlgorítimoOtimizacao/Genético.py
@@ -11,8 +11,8 @@ from random import randint
 MINIMUM = 0
 MAXIMUM = 100
 
-INDIVIDUAL_SIZE = 10
-POPULATION_SIZE = 10
+INDIVIDUAL_SIZE = 5
+POPULATION_SIZE = 5
 
 def gen_vector(min_val,max_val,individual_size):
     vector = []
@@ -26,19 +26,32 @@ def gen_population(population_size,min_val,max_val,individual_size):
         population.append(gen_vector(min_val,max_val,individual_size))
     return population
 
-def individual_cost(population,index):
+def individual_cost(individual):
     sum = 0
-    for i in population[index]:
+    for i in individual:
         sum += i
     return sum
 
 def print_population(population):
-    index = 0
     for individual in population:
-        print(individual, individual_cost(population,index))
-        index +=1
+        print(individual, individual_cost(individual))
+
+def best_individual(population):
+    best = None
+    best_sum = 1000
+    for individual in population:
+        if individual_cost(individual) < best_sum:
+            best = individual
+            best_sum = individual_cost(individual)
+    return best
+
 
 population = gen_population(POPULATION_SIZE,MINIMUM,MAXIMUM,INDIVIDUAL_SIZE)
 print_population(population)
+
+best_individual = best_individual(population)
+
+print("\nBest:",best_individual, individual_cost(best_individual))
+
 
 
