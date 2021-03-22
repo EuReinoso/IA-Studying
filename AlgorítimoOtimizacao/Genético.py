24bed543404e3958b567ee5b2117c200607d7846
EuReinoso <lucas.reinoso.br@gmail.com>
2021-03-22 20:04:37 -0300

    Final

--- AlgorítimoOtimizacao/Genético.py
+++ AlgorítimoOtimizacao/Genético.py
@@ -5,13 +5,13 @@ O objetivo desse algorítimo vai ser:
 Dada uma lista de vetores alearótios, o resultado da soma de seus componentes
 tem que ser o menor possível. Ou seja, o melhor resultado seria [0,0,0,0...]
 """
-import random,numpy
+import random,numpy,time
 
 MINIMUM = 0
 MAXIMUM = 100
 
-INDIVIDUAL_SIZE = 5
-POPULATION_SIZE = 5
+INDIVIDUAL_SIZE = 10
+POPULATION_SIZE = 100
 
 def gen_vector(individual_size):
     vector = []
@@ -35,7 +35,7 @@ def print_population(population):
     for individual in population:
         print(individual, individual_cost(individual))
 
-def best_individual(population):
+def best_select(population):
     best = None
     best_sum = 1000
     for individual in population:
@@ -56,22 +56,23 @@ def mutation(individual):
 def gen_new_population(individual):
     population = []
     for i in range(POPULATION_SIZE):
-        random.seed()
         population.append(mutation(individual))
     return population
 
 
 population = gen_population(POPULATION_SIZE,INDIVIDUAL_SIZE)
-best_individual = best_individual(population)
+best_individual = best_select(population)
 
 print_population(population)
-print()
-print(best_individual,individual_cost(best_individual))
-print()
-print("\n", print_population(gen_new_population(best_individual)))
 
-# while individual_cost(best_individual) > 0:
-#     print("\nBest:",best_individual, individual_cost(best_individual))
+while individual_cost(best_individual) > 0:
+    population = gen_new_population(best_individual)
+    best_individual = best_select(population)
+
+    print("\nBest:",best_individual, individual_cost(best_individual))
+    time.sleep(1)
+
+
 
 
 
