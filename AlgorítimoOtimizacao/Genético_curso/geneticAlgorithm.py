d7b7e82416f68153f11a9c6938a4b94aa187f4eb
EuReinoso <lucas.reinoso.br@gmail.com>
2021-03-24 12:30:11 -0300

    Population assessment

--- AlgorítimoOtimizacao/Genético_curso/geneticAlgorithm.py
+++ AlgorítimoOtimizacao/Genético_curso/geneticAlgorithm.py
@@ -6,7 +6,6 @@ class geneticAlgorithm:
         self.generation = 0
         self.population = []
         self.best_solution = 0
-        self.grade = 0
 
     def population_init(self,spaces,valors,spaces_limit):
         for i in range(self.population_size):
@@ -21,3 +20,9 @@ class geneticAlgorithm:
     def best_individual(self,invdividual):
         if individual.grade > self.best_solution.grade:
             self.best_solution = individual
+
+    def population_assessment(self):
+        grade = 0
+        for individual in self.population:
+            grade += individual.grade
+        return grade
