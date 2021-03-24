c8ac9823b036a722b08c817e9c86e07da71d66d1
EuReinoso <lucas.reinoso.br@gmail.com>
2021-03-24 10:18:46 -0300

    Assessment

--- AlgorítimoOtimizacao/Genético_curso/individual.py
+++ AlgorítimoOtimizacao/Genético_curso/individual.py
@@ -7,6 +7,7 @@ class Individual:
         self.spaces_limit = spaces_limit
         self.generation = generation
         self.grade = 0
+        self.total_space = 0
         self.chromosome = []
 
         for i in range(len(spaces)):
@@ -14,3 +15,18 @@ class Individual:
                 self.chromosome.append("0")
             else:
                 self.chromosome.append("1")
+    
+    def assessment(self):
+        total_space = 0
+        grade = 0
+        for i in range(len(self.chromosome)):
+            if self.chromosome[i] == '1':
+                total_space += self.spaces[i]
+                grade += self.valors[i]
+                
+        if total_space > self.spaces_limit:
+            grade = 1
+
+        self.total_space = total_space
+        self.grade = grade
+
