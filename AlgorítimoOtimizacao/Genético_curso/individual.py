eb0780af2914fa6cfd66aed8ead464e9297ea2fb
EuReinoso <lucas.reinoso.br@gmail.com>
2021-03-24 10:24:18 -0300

    crossover

--- AlgorítimoOtimizacao/Genético_curso/individual.py
+++ AlgorítimoOtimizacao/Genético_curso/individual.py
@@ -23,10 +23,24 @@ class Individual:
             if self.chromosome[i] == '1':
                 total_space += self.spaces[i]
                 grade += self.valors[i]
-                
+
         if total_space > self.spaces_limit:
             grade = 1
 
         self.total_space = total_space
         self.grade = grade
+    
+    def crossover(self,other):
+        cut = round(random() * len(self.chromosome))
+
+        son1 = other.chromosome[0:cut] + self.chromosome[cut::]
+        son2 = self.chromosome[0:cut] + other.chromosome[cut::]
+
+        sons = [Individual(self.spaces,self.valors,self.spaces_limit),
+                Individual(self.spaces,self.valors,self.spaces_limit)]
+        
+        sons[0].chromosome = son1
+        sons[1].chromosome = son2
+
+        return sons
 
