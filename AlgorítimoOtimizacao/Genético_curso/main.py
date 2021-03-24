611ca0d5c251aab7d089aa7aa5e623d3a203c6e2
EuReinoso <lucas.reinoso.br@gmail.com>
2021-03-24 07:26:27 -0300

    Product and product_list

---
+++ AlgorítimoOtimizacao/Genético_curso/main.py
@@ -0,0 +1,18 @@
+from product import Product
+
+product_list = []
+
+product_list.append(Product("Geladeira Dako", 0.751, 999.90))
+product_list.append(Product("Iphone 6", 0.0000899, 2911.12))
+product_list.append(Product("TV 55' ", 0.400, 4346.99))
+product_list.append(Product("TV 50' ", 0.290, 3999.90))
+product_list.append(Product("TV 42' ", 0.200, 2999.00))
+product_list.append(Product("Notebook Dell", 0.00350, 2499.90))
+product_list.append(Product("Ventilador Panasonic", 0.496, 199.90))
+product_list.append(Product("Microondas Electrolux", 0.0424, 308.66))
+product_list.append(Product("Microondas LG", 0.0544, 429.90))
+product_list.append(Product("Microondas Panasonic", 0.0319, 299.29))
+product_list.append(Product("Geladeira Brastemp", 0.635, 849.00))
+product_list.append(Product("Geladeira Consul", 0.870, 1199.89))
+product_list.append(Product("Notebook Lenovo", 0.498, 1999.90))
+product_list.append(Product("Notebook Asus", 0.527, 3999.00))
